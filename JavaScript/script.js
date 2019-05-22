a9a1212ead144f091ad4f6ac13e0905b6513bc95
Dave <redirwin@gmail.com>
2019-05-22 11:39:12 -0700

    WIP - Made logo clickable to home page.

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+
+
+// EVent listener to redirect to home when logo image is clicked
+
+const clickHome = document.querySelector('.click-home');
+
+clickHome.addEventListener('click', () => {
+    window.location.replace('index.html');
+})
