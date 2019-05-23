2c7f4c48d365a82a5c8bef1d79c789e19bd7bb18
Dave <redirwin@gmail.com>
2019-05-22 17:23:07 -0700

    Prettified code on multiple pages.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,7 @@
-
-
 // EVent listener to redirect to home when logo image is clicked
 
 const clickHome = document.querySelector('.click-home');
 
 clickHome.addEventListener('click', () => {
-    window.location.replace('index.html');
-})
+	window.location.replace('index.html');
+});
